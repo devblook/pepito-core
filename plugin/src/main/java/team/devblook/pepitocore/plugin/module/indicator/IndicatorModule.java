4811f13f02e83ac40fab0787d37abab4b5a0e113
siansxint <joshua1504.jr@gmail.com>
2024-05-13 22:45:53 -0400

    asd

--- plugin/src/main/java/team/devblook/pepitocore/plugin/module/indicator/IndicatorModule.java
+++ plugin/src/main/java/team/devblook/pepitocore/plugin/module/indicator/IndicatorModule.java
@@ -5,12 +5,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.Plugin;
 import team.devblook.pepitocore.api.module.CoreModule;
-import team.devblook.pepitocore.plugin.module.sleep.listener.SkipSleepListener;
+import team.devblook.pepitocore.plugin.module.indicator.listener.EntityDamageListener;
 
 public class IndicatorModule implements CoreModule {
 
     private @Inject Plugin plugin;
-    private @Inject SkipSleepListener listener;
+    private @Inject EntityDamageListener listener;
 
     @Override
     public void enable() {
@@ -24,5 +24,4 @@ public class IndicatorModule implements CoreModule {
     public void disable() {
         HandlerList.unregisterAll(listener);
     }
-
 }
--- plugin/src/main/java/team/devblook/pepitocore/plugin/module/indicator/listener/EntityDamageListener.java
+++ plugin/src/main/java/team/devblook/pepitocore/plugin/module/indicator/listener/EntityDamageListener.java
@@ -4,38 +4,56 @@ import com.google.inject.Inject;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
-import org.bukkit.entity.LivingEntity;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
-import team.devblook.pepitocore.plugin.module.indicator.util.IndicatorUtil;
 
 public class EntityDamageListener implements Listener {
+
+    private static final NamespacedKey INDICATOR_KEY = new NamespacedKey("bc", "indicator");
 
     private @Inject Plugin plugin;
 
     @EventHandler(ignoreCancelled = true)
-    public void onEntityDamageByPlayer(final EntityDamageByEntityEvent event) {
+    public void onEntityDamageByPlayer(EntityDamageByEntityEvent event) {
         if (!(event.getDamager() instanceof Player player)) {
             return;
         }
 
-        if (!(event.getEntity() instanceof LivingEntity entity)) {
-            return;
-        }
-
-        final TagResolver.Single indicator = Placeholder.component(
+        TagResolver.Single indicator = Placeholder.component(
                 "damage",
                 Component.text((int) event.getFinalDamage()));
 
-        IndicatorUtil.spawnIndicator(
-                this.plugin,
-                player,
-                entity,
-                null); //this.configuration.getComponent("damage.indicator-format", indicator));
+        ArmorStand stand = (ArmorStand) player.getWorld()
+                .spawnEntity(
+                        event.getEntity().getLocation()
+                                .add(0, 1, 0),
+                        EntityType.ARMOR_STAND
+                );
+
+        stand.setInvisible(true);
+        stand.setInvulnerable(true);
+        stand.setMarker(true);
+        stand.setCustomNameVisible(true);
+        stand.customName(null); //this.configuration.getComponent("damage.indicator-format", indicator));
+        stand.getPersistentDataContainer().set(
+                INDICATOR_KEY,
+                PersistentDataType.STRING,
+                "active"
+        );
+
+        Bukkit.getScheduler()
+                .runTaskLater(
+                        plugin,
+                        stand::remove,
+                        20L
+                );
     }
-
-
 }
