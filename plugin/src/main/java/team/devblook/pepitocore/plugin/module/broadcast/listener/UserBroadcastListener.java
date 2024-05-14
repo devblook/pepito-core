0e5d492d09650e32c86821c5e045d9683ad75a49
jonakls <asmot54@hotmail.com>
2024-05-13 20:56:12 -0500

    refactor(message): edit join and quit message

--- plugin/src/main/java/team/devblook/pepitocore/plugin/module/broadcast/listener/UserBroadcastListener.java
+++ plugin/src/main/java/team/devblook/pepitocore/plugin/module/broadcast/listener/UserBroadcastListener.java
@@ -2,6 +2,7 @@ package team.devblook.pepitocore.plugin.module.broadcast.listener;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,8 +16,8 @@ public class UserBroadcastListener implements Listener {
         final Player player = event.getPlayer();
 
         final Component joinMessage = Component.text()
-                .append(Component.text("✓ ", TextColor.color(0x43AE33)))
-                .append(Component.text(player.getName()))
+                .append(Component.text("✓ ", TextColor.color(0x43AE33)).decorate(TextDecoration.BOLD))
+                .append(Component.text(player.getName(), TextColor.color(0xDFFFEF)))
                 .append(Component.text(" ingresó al servidor.", TextColor.color(0xA3C18F)))
                 .build();
 
@@ -28,8 +29,8 @@ public class UserBroadcastListener implements Listener {
         final Player player = event.getPlayer();
 
         final Component quitMessage = Component.text()
-                .append(Component.text("✕ ", TextColor.color(0xAE1F20)))
-                .append(Component.text(player.getName()))
+                .append(Component.text("✕ ", TextColor.color(0xAE1F20)).decorate(TextDecoration.BOLD))
+                .append(Component.text(player.getName(), TextColor.color(0xDFFFEF)))
                 .append(Component.text(" salió al servidor.", TextColor.color(0xA3C18F)))
                 .build();
 
