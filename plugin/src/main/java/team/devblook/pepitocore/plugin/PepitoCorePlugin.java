fb024ce27ef5dd16dbe7161a8a4b0c41016d8713
jonakls <asmot54@hotmail.com>
2024-05-16 19:09:00 -0500

    feat(persistent): add persistent modules

--- plugin/src/main/java/team/devblook/pepitocore/plugin/PepitoCorePlugin.java
+++ plugin/src/main/java/team/devblook/pepitocore/plugin/PepitoCorePlugin.java
@@ -2,6 +2,7 @@ package team.devblook.pepitocore.plugin;
 
 import com.google.inject.Guice;
 import com.google.inject.Inject;
+import com.google.inject.Injector;
 import me.fixeddev.commandflow.CommandManager;
 import me.fixeddev.commandflow.annotated.AnnotatedCommandTreeBuilder;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,6 +10,8 @@ import team.devblook.pepitocore.api.PepitoCoreAPI;
 import team.devblook.pepitocore.api.module.CoreModule;
 import team.devblook.pepitocore.api.registry.TRegistry;
 import team.devblook.pepitocore.plugin.command.ModuleCommand;
+import team.devblook.pepitocore.plugin.configuration.BukkitConfiguration;
+import team.devblook.pepitocore.plugin.module.Modules;
 
 public final class PepitoCorePlugin
         extends JavaPlugin
@@ -20,16 +23,26 @@ public final class PepitoCorePlugin
     private @Inject ModuleCommand command;
 
     private @Inject TRegistry<String, CoreModule> modules;
+    private @Inject BukkitConfiguration configuration;
+    private Injector injector;
 
     @Override
     public void onLoad() {
-        Guice.createInjector(new PepitoCoreBindingModule(this))
-                .injectMembers(this);
+        this.injector = Guice.createInjector(new PepitoCoreBindingModule(this));
+        this.injector.injectMembers(this);
     }
 
     @Override
     public void onEnable() {
         manager.registerCommands(builder.fromClass(command));
+
+        configuration.get().getStringList("module.active-modules").forEach(value -> {
+            final CoreModule module = Modules.MAPPINGS.get(value).apply(this.injector);
+            if (module != null) {
+                modules.insert(module.id(), module);
+                module.enable();
+            }
+        });
     }
 
     @Override
