6d62f8370952a3112359c6c44b400a02d6a217c4
SEG <segdev.mx@gmail.com>
2024-06-02 02:13:13 -0600

    fix(damage-indicator): the armor stand is visible for a brief moment

--- plugin/src/main/java/team/devblook/pepitocore/plugin/module/indicator/listener/EntityDamageListener.java
+++ plugin/src/main/java/team/devblook/pepitocore/plugin/module/indicator/listener/EntityDamageListener.java
@@ -3,8 +3,6 @@ package team.devblook.pepitocore.plugin.module.indicator.listener;
 import com.google.inject.Inject;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
-import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.ArmorStand;
@@ -12,6 +10,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
@@ -30,29 +29,35 @@ public class EntityDamageListener implements Listener {
 
         int damage = (int) event.getFinalDamage();
 
-        ArmorStand stand = (ArmorStand) player.getWorld()
+        player.getWorld()
                 .spawnEntity(
                         event.getEntity().getLocation()
                                 .add(0, 1, 0),
-                        EntityType.ARMOR_STAND
-                );
+                        EntityType.ARMOR_STAND,
+                        CreatureSpawnEvent.SpawnReason.CUSTOM,
+                        entity -> {
+                            if (!(entity instanceof ArmorStand stand)) {
+                                return;
+                            }
 
-        stand.setInvisible(true);
-        stand.setInvulnerable(true);
-        stand.setMarker(true);
-        stand.setCustomNameVisible(true);
-        stand.customName(Component.text("-" + damage, TextColor.color(0xAE1F20))); //this.configuration.getComponent("damage.indicator-format", indicator));
-        stand.getPersistentDataContainer().set(
-                INDICATOR_KEY,
-                PersistentDataType.STRING,
-                "active"
-        );
+                            stand.setInvisible(true);
+                            stand.setInvulnerable(true);
+                            stand.setMarker(true);
+                            stand.setCustomNameVisible(true);
+                            stand.customName(Component.text("-" + damage, TextColor.color(0xAE1F20))); //this.configuration.getComponent("damage.indicator-format", indicator));
+                            stand.getPersistentDataContainer().set(
+                                    INDICATOR_KEY,
+                                    PersistentDataType.STRING,
+                                    "active"
+                            );
 
-        Bukkit.getScheduler()
-                .runTaskLater(
-                        plugin,
-                        stand::remove,
-                        20L
+                            Bukkit.getScheduler()
+                                    .runTaskLater(
+                                            plugin,
+                                            stand::remove,
+                                            20L
+                                    );
+                        }
                 );
     }
 }
